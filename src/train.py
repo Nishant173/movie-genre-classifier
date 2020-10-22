d3d06376778908a36c25b6cceae5eb13d1105dac
nishant173 <nishant.rao173@gmail.com>
2020-10-23 00:47:02 +0530

    Refactored train.py

--- src/train.py
+++ src/train.py
@@ -14,22 +14,30 @@ import data_reader
 import text_cleaner
 import utils
 
+def preprocess_data():
+    """Preprocess train-test data from scratch and return Pandas DataFrame of the same"""
+    df_movie_data = data_reader.get_movie_data()
+    stopwords_list = list(set(stopwords.words('english')))
+    df_movie_data['Plot'] = df_movie_data['Plot'].apply(text_cleaner.clean_text, stopwords_list=stopwords_list)
+    utils.pickle_save(data_obj=df_movie_data, filepath=config.PATH_MOVIE_DATA_PREPROCESSED)
+    return df_movie_data
+
 def get_train_test_data():
     """Returns Pandas DataFrame of preprocessed train-test movie plot/genre data"""
     if config.LOAD_PREPROCESSED_DATA:
         try:
             df_movie_data = utils.pickle_load(filepath=config.PATH_MOVIE_DATA_PREPROCESSED)
         except FileNotFoundError:
-            raise Exception("Pickle of preprocessed data not found. Please set config.LOAD_PREPROCESSED_DATA to False (for now), and redo data preprocessing")
+            df_movie_data = preprocess_data()
     else:
-        df_movie_data = data_reader.get_movie_data()
-        stopwords_list = list(set(stopwords.words('english')))
-        df_movie_data['Plot'] = df_movie_data['Plot'].apply(text_cleaner.clean_text, stopwords_list=stopwords_list)
-        utils.pickle_save(data_obj=df_movie_data, filepath=config.PATH_MOVIE_DATA_PREPROCESSED)
+        df_movie_data = preprocess_data()
     return df_movie_data
 
 def train_model(df_movie_data):
-    """Trains model and returns dictionary containing info about said model"""
+    """
+    Trains classification model and returns dictionary containing info about said model.
+    Also saves the necessary model-related files aptly.
+    """
     mlb = MultiLabelBinarizer()
     X = df_movie_data['Plot']
     y = mlb.fit_transform(df_movie_data['Genre'])
@@ -44,11 +52,10 @@ def train_model(df_movie_data):
     ovr_clf = OneVsRestClassifier(lr)
     ovr_clf.fit(X=X_train, y=y_train)
     y_pred = ovr_clf.predict(X=X_test)
-
+    # Saving necessary model-related files
     utils.pickle_save(data_obj=mlb, filepath=config.PATH_MODEL_MLB)
     utils.pickle_save(data_obj=tfidf, filepath=config.PATH_MODEL_TFIDF)
     utils.pickle_save(data_obj=ovr_clf, filepath=config.PATH_MODEL_OVR_CLF)
-    
     dictionary_train_info = {
         'X_train': X_train,
         'X_test': X_test,
@@ -60,7 +67,7 @@ def train_model(df_movie_data):
 
 def evaluate_model(ovr_clf, dictionary_train_info):
     """
-    Evaluates the model based on it's classifier object and some training info.
+    Evaluates the model based on it's classifier object and certain training info.
     Returns dictionary of the model's evaluation metrics.
     """
     thresh = 0.3
