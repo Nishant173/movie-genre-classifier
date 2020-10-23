29ad288608104b636a105b09a050164123ad8f3b
nishant173 <nishant.rao173@gmail.com>
2020-10-23 13:05:07 +0530

    Added dictionary of HYPERPARAMS_TO_TRY

--- src/config.py
+++ src/config.py
@@ -7,4 +7,10 @@ PATH_MODEL_MLB = "../model_files/mlb_obj.pkl"
 PATH_MODEL_TFIDF = "../model_files/tfidf_obj.pkl"
 PATH_MODEL_OVR_CLF = "../model_files/ovr_clf_obj.pkl"
 
-PATH_MODEL_EVAL_METRICS = "../model_files/model_evaluation_metrics.pkl"
+PATH_HYPERPARAMS_TRIED = "../model_files/hyperparams_tried.csv"
+
+# Hyperparams that need to be tried out for the model (before selecting the best set of hyperparams)
+HYPERPARAMS_TO_TRY = {
+    'max_df': [0.65, 0.7, 0.75, 0.8, 0.85],
+    'max_features': [7000, 8000, 9000, 10000, 11000],
+}
