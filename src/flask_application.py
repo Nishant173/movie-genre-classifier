625447e2822d7f960e0342b545ca4ac918d12e7b
nishant173 <nishant.rao173@gmail.com>
2020-10-23 16:38:29 +0530

    Added Flask form and template

---
+++ src/flask_application.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, jsonify, request
+from predict import predict_genres_from_plot
+from utils import prettify_genres
+
+app = Flask(__name__)
+
+@app.route(rule='/', methods=['GET'])
+def home():
+    return "<h1>This is the Home page. Please go to '/predict_genres'</h1>"
+
+@app.route('/predict_genres', methods=['GET', 'POST'])
+def predict_genres():
+    if request.method == 'POST':
+        text = request.form.get('movie-plot', default="")
+        tuple_genres = predict_genres_from_plot(text=text)
+        message = prettify_genres(tuple_genres=tuple_genres)
+        response = {"message": message, "status_code": 200}
+        return jsonify(response), 200
+    return render_template(template_name_or_list='prediction_form.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
